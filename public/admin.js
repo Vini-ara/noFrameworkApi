eddd485a1605a28e52fde69bb0ab2cdc49b9664a
vinicius araujo <vinicius.ar02@gmail.com>
2022-12-29 23:52:15 -0300

    fix/refactoring finished, fixing bugs

--- public/admin.js
+++ public/admin.js
@@ -28,40 +28,54 @@ class BasicDomElemnts {
 
 class SectionNavigation extends BasicDomElemnts {
   constructor() {
+    super()
+
     this.addButton = document.getElementById("insertSelector");
     this.editButton =  document.getElementById("editSelector");
-    this.addButton.addEventListener('click', this.SelectAddSection);
-    this.editButton.addEventListener('click', this.SelectEditSection);
+
+    this.setHandlers()
+  }
+
+  setHandlers() {
+    this.addButton?.addEventListener('click', this.SelectAddSection);
+    this.editButton?.addEventListener('click', this.SelectEditSection);
   }
 
   SelectEditSection() {
-    this.addButton.classList.remove("active") 
-    this.editButton.classList.add("active")
+    this.addButton?.classList.remove("active") 
+    this.editButton?.classList.add("active")
 
-    this.editPhotoSection.classList.add("visible")
-    this.addPhotoSection.classList.remove("visible")
+    this.editPhotoSection?.classList.add("visible")
+    this.addPhotoSection?.classList.remove("visible")
   }
 
   SelectAddSection() {
-    this.addButton.classList.add("active") 
-    this.editButton.classList.remove("active")
+    this.addButton?.classList.add("active") 
+    this.editButton?.classList.remove("active")
 
-    this.editPhotoSection.classList.remove("visible")
-    this.addPhotoSection.classList.add("visible")
+    this.editPhotoSection?.classList.remove("visible")
+    this.addPhotoSection?.classList.add("visible")
   }
 }
 
 class EditSection {
   constructor(sectionNavigation) {
     this.sectionNavigation = sectionNavigation
-    this.sectionNavigation.editButton.addEventListener('click', this.MountEditSection)
+
+    this.setHandlers();
+  }
+
+  setHandlers() {
+    this.sectionNavigation.editButton.addEventListener('click', function() {
+      this.MountEditSection()
+    }.bind(this), false)
   }
 
   async MountEditSection() {     
     const images = await Api.get();
 
     images.forEach((post) => {
-      CardCreation(post, this.sectionNavigation.editPhotoSection);
+      this.CardCreation(post, this.sectionNavigation?.editPhotoSection);
     });
   }
 
@@ -100,8 +114,9 @@ class EditSection {
 }
 
 class Form {
-  constructor(suggestions) {
+  constructor(suggestions, addedPhotosBuffer) {
     this.suggestions = suggestions
+    this.addedPhotosBuffer = addedPhotosBuffer
 
     this.formElement = document.getElementById('photo-form') 
     this.photoInput = document.getElementById('file-upload')
@@ -111,6 +126,21 @@ class Form {
     this.previewContainer = document.getElementById('file-upload-container')
     this.previewImg = document.createElement('img')
     this.previewImgWrapper = document.createElement('span')
+
+    this.setHandlers()
+  }
+
+  setHandlers() {
+    this.formElement.addEventListener('submit', this.handleFormSubmission)
+    this.photoInput.addEventListener('input', this.handlePhotoInput)
+
+    this.commonNameInput.addEventListener('focus', this.handleCommonNameFocus)
+    this.commonNameInput.addEventListener('input', this.handleCommonNameInput)
+
+    this.previewImgWrapper.addEventListener('click', this.handleImgWrapperClick)
+
+    this.suggestions.suggestionsContainer.addEventListener('click', this.handleSuggestionClick)
+    document.addEventListener('click', this.handleSuggestionClose)
   }
 
   handlePhotoInput(e) {
@@ -163,9 +193,84 @@ class Form {
     this.previewContainer.removeChild(this.previewImgWrapper)
   }
 
-  clear() {
+  handleSuggestionClick(e) {
+    if(e.target.nodeName.toLowerCase() === 'i') {
+      this.scientificNameInput.value = e.target.innerText;
+      this.commonNameInput.value = e.target.parentElement.innerText.split('\n')[0];
+    } else {
+      this.scientificNameInput.value = e.target.innerText.split('\n')[1];
+      this.commonNameInput.value = e.target.innerText.split('\n')[0];
+    }
+
+    this.suggestion.suggestionsContainer.element.classList.remove('active')
+  }
+
+  handleSuggestionClose(e) {
+    let clickXstart = e.target.getBoundingClientRect().x;
+    let clickXend = e.target.getBoundingClientRect().x + e.target.getBoundingClientRect().width;
+    let clickYstart = e.target.getBoundingClientRect().y;
+    let clickYend = e.target.getBoundingClientRect().y + e.target.getBoundingClientRect().height;
+
+    let safeZoneXstart = this.suggestions?.suggestionsContainer?.getBoundingClientRect().x;
+    let safeZoneXend = this.suggestions?.suggestionsContainer?.getBoundingClientRect().x + this.suggestions?.suggestionsContainer?.getBoundingClientRect().width;
+    let safeZoneYstart = this.commonNameInput?.getBoundingClientRect().y;
+    let safeZoneYend = this.suggestions?.suggestionsContainer?.getBoundingClientRect().y + this.suggestions?.suggestionsContainer?.getBoundingClientRect().height;
+
+    if(!(clickXstart >= safeZoneXstart && clickXend <= safeZoneXend && clickYstart >= safeZoneYstart && clickYend <= safeZoneYend)) {
+      this.suggestions?.suggestionsContainer.classList.remove('active')
+    }
+  }
+
+  handleFormSubmission(e) {
+    e.preventDefault();
+    
+    let fields = {
+      commonName: this.commonNameInput.value,
+      scientificName: this.scientificNameInput.value,
+      image: this.previewImg.src
+    }
+
+    this.addedPhotosBuffer.queue.push(fields)
+
+    const addedImg = document.createElement('img')
+    addedImg.src = fields.image;
+    addedImg.alt = "image to be submitted"
+
+    let addedImgWrapper = document.createElement('span');
+    addedImgWrapper.classList.add('img-preview-wrapper');
+    addedImgWrapper.classList.add(`${this.addedPhotosBuffer.queue.length - 1}`);
+    addedImgWrapper.appendChild(addedImg);
+
+    addedImgWrapper.addEventListener('click', (e) => {
+      const listIdx = Number(e.target.classList[1]);
+
+      if(listIdx == NaN) return;
+
+      this.addedPhotosBuffer.queue.splice(listIdx, 1, '');
+
+      this.addedPhotosBuffer.checkIsListEmpty();
+
+      this.addedPhotosBuffer.vizualizationContainer.removeChild(e.target);
+    })
+    
+    this.addedPhotosBuffer.vizualizationContainer.classList.remove("empty");
+    this.addedPhotosBuffer.vizualizationContainer.appendChild(addedImgWrapper);
+
+    this.previewContainer.removeChild(this.previewImgWrapper);
+
+    this.suggestions.matches = [];
+
+    this.clear();
+
+    this.addedPhotosBuffer.checkIsListEmpty();
+
+    if(!this.addedPhotosBuffer.isListEmpty) this.addedPhotosBuffer.submitButton.disabled = false; 
+  }
+
+
+  static clear() {
     this.formElement.reset()
-    this.suggestions.matches = []
+    // this.suggestions.matches = []
   }   
 }
 
@@ -200,225 +305,75 @@ class FormSuggestions {
   }
 }
 
+class AddedPhotosBuffer {
+  constructor() {
+    this.queue = []
+    this.isEmpty = true
 
-const Form = {
-  form: document.getElementById('photo-form'),
-  photo: {
-    element: document.getElementById('file-upload'),
-    input: e => {
-      if(e.target.files.length === 0) return;
+    this.vizualizationContainer = document.getElementById('images-submitted-preview')
+    this.submitButton = document.getElementById('submit-all')
 
-      var reader = new FileReader();
-
-      reader.onload = function(a) {
-        Form.preview.img.element.src = a.target.result
-      }
-
-      reader.readAsDataURL(e.target.files[0]);
-
-      Form.preview.img.element.setAttribute('alt', "image preview");
-      Form.preview.img.element.classList.add("img-input-preview")
-
-      Form.preview.imgWrapper.element.append(Form.preview.img.element)
-
-      Form.preview.container.element.appendChild(Form.preview.imgWrapper.element);
-    }
-  },
-  commonName: {
-    element: document.getElementById('bird-name'),
-    focus: () => {
-      if(Form.suggestion.matches.length > 0) Form.suggestion.container.element.classList.add('active');
-    },
-    input: e => {
-      Form.suggestion.matches = [];
-
-      if(e.target.value.length < 2) {
-        Form.suggestion.container.element.classList.remove('active');
-        return;
-      }
-
-      birds.forEach(bird => {
-        if(bird.common.includes(e.target.value) && Form.suggestion.matches.length < 10)
-          Form.suggestion.matches.push(bird);
-      });
-
-      populateSuggestion(Form.suggestion.matches);
-
-      if(Form.suggestion.matches.length > 0) 
-        Form.suggestion.container.element.classList.add('active');
-      else 
-        Form.suggestion.container.element.classList.remove('active');
-    },
-  },
-  scientificName: {
-    element: document.getElementById('scientific-name'),
-  },
-  preview: {
-    container: {
-      element: document.getElementById('file-upload-container')
-    },
-    imgWrapper: { 
-      element: document.createElement('span'),
-      click: () => {
-        Form.photo.element.value = "";
-
-        Form.preview.imgWrapper.element.removeChild(Form.preview.img.element);
-        Form.preview.container.element.removeChild(Form.preview.imgWrapper.element);
-      }
-    },
-    img: {
-      element: document.createElement('img'),
-    },
-  }, 
-  suggestion: {
-    container: {
-      element: document.getElementById('suggestion'),
-      click: e => {
-        if(e.target.nodeName.toLowerCase() === 'i') {
-          Form.scientificName.element.value = e.target.innerText;
-          Form.commonName.element.value = e.target.parentElement.innerText.split('\n')[0];
-        } else {
-          Form.scientificName.element.value = e.target.innerText.split('\n')[1];
-          Form.commonName.element.value = e.target.innerText.split('\n')[0];
-        }
-
-        Form.suggestion.container.element.classList.remove('active')
-      }
-    },
-    matches: [],
-  },
-  clear: () => {
-    Form.form.reset();
-    Form.suggestion.matches = [];
+    this.setHandlers()
   }
-};
 
-const AddedPhotos = {
-  container: document.getElementById('images-submitted-preview'),
-  list: [],
-  isListEmpty: true,
-  checkIsListEmpty: () => {
-    let isEmpty = true; 
+  setHandlers() {
+    this.submitButton.addEventListener('click', this.handleSubmit)
+  }
 
-    AddedPhotos.list.forEach(e => {
-      if(typeof(e) === "object") isEmpty = false;
-    })
+  checkIsListEmpty() {
+    this.queue.forEach(e => {
+      if(typeof(e) === "object") this.isEmpty = false
+    }) 
 
-    if(isEmpty) {
-      AddedPhotos.list = [];
-      AddedPhotos.container.classList.add("empty");
-      AddedPhotos.submit.element.disabled = true;
+    if(this.isEmpty) {
+      this.vizualizationContainer.classList.add("empty")
+      this.submitButton.disabled = true
     }
+  }
 
-    AddedPhotos.isListEmpty = isEmpty;
-  },
-  submit: {
-    element: document.getElementById('submit-all'),
-    click: () => {
-      const data = JSON.stringify(AddedPhotos.list);
-      Api.post(data);
-      AddedPhotos.clear();
+  handleSubmit() {
+    const data = JSON.stringify(this.list)
 
-      Form.clear();
-    } 
-  },
-  clear: () => {
-    AddedPhotos.list = [];
-    AddedPhotos.checkIsListEmpty();
+    Api.post(data)
 
-    while(AddedPhotos.container.lastElementChild.tagName === "SPAN") {
-      AddedPhotos.container.removeChild(AddedPhotos.container.lastElementChild)
+    this.clear()
+
+    Form.clear()
+  }
+
+  clear() {
+    this.list = []
+    this.checkIsListEmpty()
+
+    while(this.vizualizationContainer.lastElementChild.tagName === "SPAN") {
+      this.vizualizationContainer.removeChild(this.vizualizationContainer.lastElementChild)
     }
   }
 }
 
-const App = {
-  setUp: () => {
-    document.addEventListener('click', e => {
-      let clickXstart = e.target.getBoundingClientRect().x;
-      let clickXend = e.target.getBoundingClientRect().x + e.target.getBoundingClientRect().width;
-      let clickYstart = e.target.getBoundingClientRect().y;
-      let clickYend = e.target.getBoundingClientRect().y + e.target.getBoundingClientRect().height;
 
-      let safeZoneXstart = Form.suggestion.container.element.getBoundingClientRect().x;
-      let safeZoneXend = Form.suggestion.container.element.getBoundingClientRect().x + Form.suggestion.container.element.getBoundingClientRect().width;
-      let safeZoneYstart = Form.commonName.element.getBoundingClientRect().y;
-      let safeZoneYend = Form.suggestion.container.element.getBoundingClientRect().y + Form.suggestion.container.element.getBoundingClientRect().height;
+class App {
+  constructor() {
+    this.sectionNavigation = new SectionNavigation()
+    this.editSection = new EditSection(this.sectionNavigation)
 
-      if(!(clickXstart >= safeZoneXstart && clickXend <= safeZoneXend && clickYstart >= safeZoneYstart && clickYend <= safeZoneYend)) {
-        Form.suggestion.container.element.classList.remove('active')
-      }
-    })
+    this.addedPhotosBuffer = new AddedPhotosBuffer()
 
-    Form.commonName.element.addEventListener('focusin', Form.commonName.focus)
+    this.formSuggestions = new FormSuggestions()
 
-    Form.commonName.element.addEventListener('input', Form.commonName.input)
+    this.form = new Form(this.formSuggestions, this.addedPhotosBuffer)
 
-    Form.suggestion.container.element.addEventListener('click', Form.suggestion.container.click)
+    this.birds = []
 
-    Form.photo.element.addEventListener('input', Form.photo.input)
+    this.init()
+  }
 
-    Form.preview.imgWrapper.element.addEventListener('click', Form.preview.imgWrapper.click);
-
-    Form.form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      
-      let fields = {
-        commonName: Form.commonName.element.value,
-        scientificName: Form.scientificName.element.value,
-        image: Form.preview.img.element.src
-      }
-
-      AddedPhotos.list.push(fields)
-
-      const addedImg = document.createElement('img')
-      addedImg.src = fields.image;
-      addedImg.alt = "image to be submitted"
-
-      let addedImgWrapper = document.createElement('span');
-      addedImgWrapper.classList.add('img-preview-wrapper');
-      addedImgWrapper.classList.add(`${AddedPhotos.list.length - 1}`);
-      addedImgWrapper.appendChild(addedImg);
-
-      addedImgWrapper.addEventListener('click', (e) => {
-        const listIdx = Number(e.target.classList[1]);
-
-        if(listIdx == NaN) return;
-
-        AddedPhotos.list.splice(listIdx, 1, '');
-
-        AddedPhotos.checkIsListEmpty();
-
-        AddedPhotos.container.removeChild(e.target);
-      })
-      
-      AddedPhotos.container.classList.remove("empty");
-      AddedPhotos.container.appendChild(addedImgWrapper);
-
-      Form.preview.container.element.removeChild(Form.preview.imgWrapper.element);
-
-      Form.suggestion.matches = [];
-
-      Form.clear();
-
-      AddedPhotos.checkIsListEmpty();
-
-      if(!AddedPhotos.isListEmpty) AddedPhotos.submit.element.disabled = false; 
-    })
-
-    Page.nav.insert.element.addEventListener('click', Page.nav.insert.click);
-
-    Page.nav.edit.element.addEventListener('click', Page.nav.edit.click);
-
-    AddedPhotos.submit.element.addEventListener('click', AddedPhotos.submit.click);
-  },
-  init: async () => {
+  async init() {
     let birdData = await fetch('brazilBirds.json').then(response => response.json());
-    birds = birdData.birds
-    App.setUp();
+    this.birds = birdData.birds
   }
 }
 
+const app = new App();
 
-
-await App.init();
+birds = app.birds
